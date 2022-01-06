0191df0a9f1279386c7c0520933c7a2d7e391ad6
Antoine Brique <antoine.brique@gmail.com>
2022-01-06 16:49:46 +0100

    install style to Left and Right components

---
+++ src/components/left.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Left extends Component {
+  render() {
+    return (
+      <div className="left">
+        <p>Partie gauche</p>
+      </div>
+    )
+  }
+}
+
+export default Left;
---
+++ src/components/right.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Right extends Component {
+  render() {
+    return (
+      <div className="right">
+        <p>Partie droite</p>
+      </div>
+    )
+  }
+}
+
+export default Right;
