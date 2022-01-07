d70a7720c2dcf83a76029cae872f11018a25eb9f
Antoine Brique <antoine.brique@gmail.com>
2022-01-07 12:00:55 +0100

    remove fontawesome

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Search extends Component {
   render() {
     return (
-    <input type="text" id="search" class="form-control" />
+      <input type="text" id="search" class="form-control form-search" />
     )
   }
 }
